f6ebe69d99cb1d9cad1dd01ccbf62e23f7bd177b
Anna Larch <anna@nextcloud.com>
2026-03-19 21:16:42 +0100

    fix: Replace NcAppNavigationSettings with NcAppNavigationItem for settings navigation

--- src/components/DaySidebar.vue
+++ src/components/DaySidebar.vue
@@ -11,7 +11,17 @@
 			</NcButton>
 		</template>
 		<template #footer>
-			<NcAppNavigationSettings :name="t('calorietracker', 'Daily goals')" @click="$router.push('/settings')" />
+			<NcAppNavigationItem
+				:name="t('calorietracker', 'Daily goals')"
+				:class="{ active: $route.path === '/settings' }"
+				@click="$router.push('/settings')">
+				<template #icon>
+					<svg xmlns="http://www.w3.org/2000/svg" class="day-sidebar__week-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+						<circle cx="12" cy="12" r="3" />
+						<path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
+					</svg>
+				</template>
+			</NcAppNavigationItem>
 		</template>
 		<template #list>
 			<NcAppNavigationItem
@@ -54,7 +64,6 @@
 <script>
 import NcAppNavigation from '@nextcloud/vue/dist/Components/NcAppNavigation.js'
 import NcAppNavigationItem from '@nextcloud/vue/dist/Components/NcAppNavigationItem.js'
-import NcAppNavigationSettings from '@nextcloud/vue/dist/Components/NcAppNavigationSettings.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import { mapState } from 'vuex'
 import { toLocalDateString } from '../utils/date.js'
@@ -62,7 +71,7 @@ import { toLocalDateString } from '../utils/date.js'
 export default {
 	name: 'DaySidebar',
 
-	components: { NcAppNavigation, NcAppNavigationItem, NcAppNavigationSettings, NcButton },
+	components: { NcAppNavigation, NcAppNavigationItem, NcButton },
 
 	computed: {
 		...mapState('foodEntries', ['currentDate', 'daySummaries']),
